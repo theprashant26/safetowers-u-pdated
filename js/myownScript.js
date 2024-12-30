1b25373455cc02b888935d32a97c8050d3bf9ada
Prashant Kumar <prashantsingh9826@gmail.com>
2024-12-30 17:23:51 +0530

    first commit

---
+++ js/myownScript.js
@@ -0,0 +1,16 @@
+function opencommuTower(evt, commutowerName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(commutowerName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
---
+++ packages.php
@@ -0,0 +1,9 @@
+  
+ <!-- <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script> -->
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3.exp&amp;key=AIzaSyCj2GrDSBy6ISeGg3aWUM4mn3izlA1wgt0&amp;signed_in%20=true&amp;libraries=places"></script>
+    
+    <script type="text/javascript">
+     
+         var markers = [
+	             
+			Table 'safetowe_safe.itineraryDays' doesn't exist
